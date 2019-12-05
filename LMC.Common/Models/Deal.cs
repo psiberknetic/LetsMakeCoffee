f3b421af386c6aa003436e42fee4d2cdbe109496
Nate Adams <psiberknetic@gmail.com>
2019-12-05 15:43:53 -0600

    Implement Deal

--- LMC.Common/Models/Deal.cs
+++ LMC.Common/Models/Deal.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LMC.Common
+﻿namespace LMC.Common
 {
     public class Deal
     {
+        public Deal(string code, decimal value, DealType type, decimal minimumOrder = 0.0m)
+        {
+            Code = code;
+            Value = value;
+            Type = type;
+            MinimumOrder = minimumOrder;
+        }
+
         public string Code { get; }
         public string Description { get; set; }
-        public DealType Type { get; set; }
-        public decimal Value { get; set; }
-        public decimal MinimumOrder { get; set; }
+        public DealType Type { get; }
+        public decimal Value { get; }
+        public decimal MinimumOrder { get; }
     }
 }
