f63fbdace412300d3dc8ca8f2e24dedfc334097f
Nate Adams <psiberknetic@gmail.com>
2019-12-05 15:10:27 -0600

    Implement simple DealProvider

---
+++ LMCMenuService/Controllers/DealsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LMC.Common;
+using LMC.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMC.MenuService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DealsController : ControllerBase
+    {
+        private IDealProvider _dealProvider;
+
+        public DealsController(IDealProvider dealProvider)
+        {
+            _dealProvider = dealProvider;
+        }
+
+        // GET: api/Deals
+        [HttpGet]
+        public IEnumerable<Deal> Get()
+        {
+            return _dealProvider.GetDeals();
+        }
+
+        // GET: api/Deals/Super50Friday
+        [HttpGet("{code}", Name = "Get")]
+        public Deal Get(string code)
+        {
+            return _dealProvider.GetDealByCode(code);
+        }
+    }
+}
--- LMCMenuService/Startup.cs
+++ LMCMenuService/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LMC.Common.Interfaces;
+using LMC.MenuService.Providers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,7 @@ namespace LMC.MenuService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<IDealProvider, DealProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
