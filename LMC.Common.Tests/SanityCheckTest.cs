32c9153bd0c9a6dace84d74b931ff82e305a9ca6
Nate Adams <psiberknetic@gmail.com>
2019-12-06 17:47:12 -0600

    Add stupid test to check Build pipeline

---
+++ LMC.Common.Tests/SanityCheckTest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMC.Common.Tests
+{
+    [TestClass]
+    public class SanityCheckTest
+    {
+        [TestMethod]
+        public void AlwaysPasses()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
