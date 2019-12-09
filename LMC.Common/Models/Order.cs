7dca34961a8efdf8b923b63c32fad00d024946af
Nate Adams <psiberknetic@gmail.com>
2019-12-09 10:51:19 -0600

    Add properties to Order

--- LMC.Common/Models/Order.cs
+++ LMC.Common/Models/Order.cs
@@ -9,5 +9,9 @@ namespace LMC.Common
         public string Customer { get; set; }
         public OrderStatus Status { get; set; }
         public IEnumerable<OrderLine> Lines { get; set; }
+        public Deal AppliedDeal { get; set; }
+        public decimal SubTotal { get; }
+        public decimal AmountSaved { get; }
+        public decimal Total { get; }
     }
 }
