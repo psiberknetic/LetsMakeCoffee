e71310000982c2f98113521993756f737f4d62b6
Nate Adams <psiberknetic@gmail.com>
2019-12-09 14:34:04 -0600

    Add simple MenuItemProvider

---
+++ LMC.Common/Interfaces/IMenuProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMC.Common.Interfaces
+{
+    public interface IMenuProvider
+    {
+        IEnumerable<MenuItem> GetMenuItems();
+        IEnumerable<MenuItem> GetMenuItemsByCategory(MenuCategory category);
+        MenuItem GetMenuItem(Guid id);
+    }
+}
---
+++ LMCView/Data/Providers/StaticMenuProvider.cs
@@ -0,0 +1,27 @@
+﻿using LMC.Common;
+using LMC.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace LMC.Web.Data.Providers
+{
+    public class StaticMenuProvider : IMenuProvider
+    {
+
+
+        public MenuItem GetMenuItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MenuItem> GetMenuItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MenuItem> GetMenuItemsByCategory(MenuCategory category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
