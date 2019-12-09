e2be66ad46a7c4b3995923f8271c17cfc88e7bd3
Nate Adams <psiberknetic@gmail.com>
2019-12-08 21:02:14 -0600

    Add IOrderProvidre interface

---
+++ LMC.Common/Interfaces/IOrderProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMC.Common.Interfaces
+{
+    public interface IOrderProvider
+    {
+        IEnumerable<Order> GetAllOrders();
+
+        IEnumerable<Order> GetOpenOrders();
+
+        Order GetOrder(int orderId);
+    }
+}
