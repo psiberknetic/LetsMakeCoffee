7b41c2e529f00bd4cb11df88ddd5af4057a54633
charris-greatamerica <charris@greatamerica.com>
2019-12-11 12:21:55 -0600

    Adding weekday scenerio

---
+++ LMC.Deals.Tests/DealOfTheDay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using LMC.Common;
+using LMC.Common.Interfaces;
+using LMC.Data.Providers;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace LMC.Deals.Tests
+{
+    [Binding]
+    class DealOfTheDay
+    {
+        [Then(@"I am returned only the DealOfTheDay deal")]
+        public void ThenIAmReturnedOnlyTheDealOfTheDayDeal()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
---
+++ LMC.Deals.Tests/DealOfTheDay.feature
@@ -0,0 +1,10 @@
+﻿Feature: DealOfTheDay
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Deal of the Day has to be a weekday
+	Given It is Monday
+	When I add the deal to the menu items
+	Then I apply the deal to the menu items
